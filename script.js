e9d149ac2fec005c658712e61673af5ec5fa808f
Daniel Shpagin <danielshpagin@gmail.com>
2023-01-22 16:36:54 +0200

    New update?

--- script.js
+++ script.js
@@ -15,6 +15,29 @@ function pow(x, y) {
     return Math.pow(x, y);
 }
 
+function gcd(x, y) {
+    function data() {
+        var cof = 0;
+        cof = Math.floor(x/y);
+
+        var num3 = x;
+        var num4 = y;
+
+        x = num4;
+        y = num3-Math.floor(num4*cof);
+    }
+
+    while (true) {
+        data();
+
+        if (y <= 0) {
+            break;
+        }
+    }
+
+    return x;
+}
+
 function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
